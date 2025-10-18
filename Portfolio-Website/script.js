0b3b4ca4d98dc91fd7b60ded82b972a5c2321ebc
Ganesh-creater <70442015+Ganesh-creater@users.noreply.github.com>
2025-10-18 11:41:23 +0530

    Add files via upload

---
+++ Portfolio-Website/script.js
@@ -0,0 +1,78 @@
+const navbtn = document.querySelectorAll('.nav');
+const button = document.querySelectorAll('.btn');
+const menuButton = document.querySelector('#menu-button');
+const rootElement = document.documentElement;
+const myForm = document.querySelector('.myform');
+const submit = document.querySelector('.submit');
+
+menuButton.addEventListener('click', () => {
+    rootElement.toggleAttribute('menu-open')
+})		
+navbtn.forEach(a => {
+    a.addEventListener('click', function() {
+        // Remove 'active' class from all buttons
+        navbtn.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        // Add 'active' class from all buttons
+        this.classList.add('active');
+    });
+});
+
+navbtn.forEach(a => {
+    a.addEventListener('mouseover', function() {
+        // Remove 'active' class from all buttons
+        navbtn.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        // Add 'active' class from all buttons
+        this.classList.add('active');
+    });
+});
+
+button.forEach(a => {
+    a.addEventListener('click', function() {
+        // Remove 'active' class from all buttons
+        button.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        // Add 'active' class from all buttons
+        this.classList.add('active');
+    });
+});
+button.forEach(a => {
+    a.addEventListener('mouseover', function() {
+        // Remove 'active' class from all buttons
+        button.forEach(btn => {
+            btn.classList.remove('active');
+        });
+        // Add 'active' class from all buttons
+        this.classList.add('active');
+    });
+});
+myForm.addEventListener('submit', function (event){
+    // Prevent the default form submission if yu don't want to send data
+    event.preventDefault();
+
+    myForm.submit();
+    // Clear the form fieldds
+    myForm.reset();
+});
+submit.addEventListener('submit', function(event) {
+    this.classList.remove('action');
+    this.classList.add('action');
+})
+
+// Lenis smooth scrolling
+const lenis = new Lenis();
+
+lenis.on('scroll', (e) => {
+console.log(e)
+})
+
+function raf(time) {
+lenis.raf(time)
+requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf);
